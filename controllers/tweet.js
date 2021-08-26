a9eea67c12867ee6620d1a3f523649a4583b5090
Froylan <froy.push@gmail.com>
2021-08-26 17:16:25 -0500

    Commit 4 - Created Index New and Post routes that are working

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -3,14 +3,49 @@ const Feed = require('../models/tweet')
 const router = express.Router()
 
 //index (.get) ejs
+router.get('/', ( req, res )=> {
+    Feed.find({}, ((err, feedIndex)=> {
+        if (err) {
+            res.send(err)
+        } else {
+            res.render('index.ejs', {
+                feed: feedIndex
+            })
+        }
+    }))
+})
 
 // new (.get) ejs
+router.get('/new', ( req, res )=> {
+    res.render('new.ejs')
+})
 
 //edit (.get) ejs
+router.get('/:id', ( req, res )=> {
+    Feed.findById(req.params.id, ((err, idFeed)=> {
+        if (err) {
+            res.send(err)
+        } else {
+            res.render('edit.ejs', {
+                feed: idFeed,
+                id: req.params.id
+            })
+        }
+    }))
+})
 
 //show (.get) ejs
 
 //create (.post)
+router.post('/', ( req, res )=> {
+    Feed.create(req.body, ((err, createdFeed)=> {
+        if (err) {
+            res.send(err)
+        } else {
+            res.redirect('/feed')
+        }
+    }))
+})
 
 //delete (.delete)
 
